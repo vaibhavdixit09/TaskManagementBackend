4615ee1b7dc60323a58f036b5f495d2c67d1c4b8
Vaibhav Dixit <101272913+vaibhavdixit09@users.noreply.github.com>
2024-08-08 12:57:19 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,15 +5,15 @@ const routes = require("./routes/routes");
 const app = express();
 require("dotenv").config();
 
+
+// Use cors middleware
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "https://66b46fb2a013aeaf4919348d--jazzy-kleicha-8be487.netlify.app"
-    ] // Allow requests from these origins
+    origin: "https://jazzy-kleicha-8be487.netlify.app", // Allow this origin
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Allow these methods
+    allowedHeaders: ["Content-Type", "Authorization"], // Allow these headers
   })
 );
-
 
 app.use(express.json());
 
